659474df83954b7375ef7a8a056db0bbc23549c4
Biju Punalor <biju.punalor@gmail.com>
2021-10-02 18:07:32 +0530

    sets user name and font

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "bijumon"
+      user-mail-address "")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -19,7 +19,9 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Iosevka Term" :size 20)
+;; (setq doom-font (font-spec :family "Iosevka Term" :size 20)
+;;      doom-variable-pitch-font (font-spec :family "Noto sans" :size 16))
+(setq doom-font (font-spec :family "Fira Code" :size 18)
       doom-variable-pitch-font (font-spec :family "Noto sans" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
